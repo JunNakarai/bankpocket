acd22459e8013f5b9a0f7588965dc975f8bbf067
Jun Nakarai <jun.nakarai@icloud.com>
2025-09-18 19:46:40 +0900

    Initial Commit

---
+++ bankpocketTests/bankpocketTests.swift
@@ -0,0 +1,16 @@
+//
+//  bankpocketTests.swift
+//  bankpocketTests
+//
+//  Created by Jun Nakarai on 2025/09/18.
+//
+
+import Testing
+
+struct bankpocketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
