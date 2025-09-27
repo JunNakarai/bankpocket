77e650d76a0b47b40dbf1df374039e41778d3fff
Jun Nakarai <jun.nakarai@icloud.com>
2025-09-27 18:34:08 +0900

    test: rename generated suites for lint compliance

--- bankpocketTests/bankpocketTests.swift
+++ bankpocketTests/bankpocketTests.swift
@@ -7,7 +7,7 @@
 
 import Testing
 
-struct bankpocketTests {
+struct BankpocketTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
